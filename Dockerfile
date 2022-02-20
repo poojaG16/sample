3dfa58c158bfaec6211ffa33caffafd59e852457
pooja16 <poojagaikwad28603@gmail.com>
2022-02-20 19:39:19 +0530

    app updated

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY package.json /app
 RUN npm install
 COPY . /app
 CMD node server.js
-EXPOSE 8085
+EXPOSE 3000
--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var app = express();
 
-var logger = function(req,res,next){
-    next();
-}
 
 var home=function(req,res){
     res.send("<h1>Welcome To Web</h1>"+"<hr>"+"<h3>SDMEnd Module Exam</h3>");
@@ -11,4 +8,4 @@ var home=function(req,res){
 
 app.get("/",home);
 
-app.listen(8085);
+app.listen(3000);
